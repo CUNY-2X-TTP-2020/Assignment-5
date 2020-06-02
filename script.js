6a98747afec01a72eae8dec67676da4c6c7a9400
RickyC0626 <rickychon99@gmail.com>
2020-06-02 01:01:12 -0400

    Add implementation of #9

--- script.js
+++ script.js
@@ -153,7 +153,14 @@ let currentColor = "gray";
 
 function clearAllCells()
 {
-    
+    let cells = document.getElementsByClassName("grid-cell");
+    [...cells].forEach(cell =>
+    {
+        cell.style.backgroundColor = null;
+        cell.classList.remove(...cell.classList);
+        cell.classList.add("grid-cell");
+        cell.classList.add("uncolored");
+    });
 }
 
 function fillAllCells()
