513d6c90a7bd4e6ae989be74dfdbb7f13d30809a
RickyC0626 <rickychon99@gmail.com>
2020-06-01 20:10:14 -0400

    Add simple addRow functionality for single column

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+let cells = 1;
+
 function addRow()
 {
+    let grid = document.getElementById("grid");
+    let newRow = document.createElement("tr");
+    newRow.classList.add("grid-row");
+    grid.appendChild(newRow);
 
+    for(let i = 0; i < cells; i++)
+    {
+        let cell = document.createElement("td");
+        cell.classList.add("grid-column");
+        newRow.appendChild(cell);
+    }
 }
 
 function addColumn()
@@ -15,5 +27,5 @@ function removeRow()
 
 function removeColumn()
 {
-    
+
 }
